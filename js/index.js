4263f47f9b1923fc5fd3ca449287b1f926ad41d4
Adrian <adri27informatica@gmail.com>
2022-10-05 23:06:58 +0200

    Aplicacion Ventana modal

--- js/index.js
+++ js/index.js
@@ -6,13 +6,10 @@ const btnCerrar = document.getElementById("btnCerrar");
 btnAbrirModal.addEventListener("click", abrirModal);
 
 function abrirModal() {
-  console.log("a");
   contenedorModal.classList.add("modal-open");
-  document.getElementById("container").style.opacity = 0.5;
   btnCerrar.addEventListener("click", cerrarModal);
 }
 
 function cerrarModal() {
   contenedorModal.classList.remove("modal-open");
-  document.getElementById("container").style.opacity = 1;
 }
